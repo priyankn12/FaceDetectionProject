32ba8d74dae774dd8d093db60ab0dd0585258d52
priyankn12 <44427983+priyankn12@users.noreply.github.com>
2020-04-11 13:25:07 +0530

    Add files via upload

---
+++ webcam_face.py
@@ -0,0 +1,17 @@
+import cv2
+cap=cv2.VideoCapture(0)
+face_cascade=cv2.CascadeClassifier("/Users/priyank/Desktop/Data Science Folder/Lecture8/Pycharm/haarcascade_frontalface_default.xml")
+while True:
+    ret,frame=cap.read()
+    if ret==False:
+        continue
+    faces=face_cascade.detectMultiScale(frame,1.3,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.imshow("VideoFrame", frame)
+
+    key=cv2.waitKey(1)
+    if key==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
